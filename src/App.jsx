9068e00ca7b252d9e7e0450dbe66b56e12b8b4ed
Akash <adesigns>
2023-04-13 15:41:32 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./root.css";
+
+import Entry from "./Components/Entry";
+
+const App = () => {
+    return (
+        <>
+           <Entry />
+        </>
+    )
+}
+
+export default App;
