438b81391a0c9e73b2e405a4e586f5f4a0eb0265
Akash Mulye <akashmulye97@gmail.com>
2023-07-17 20:10:25 -0400

    added back to top button

--- src/Components/Introduction/index.jsx
+++ src/Components/Introduction/index.jsx
@@ -1,8 +1,10 @@
 import React, {useEffect, useState} from "react";
 import "./introduction.css";
 import logoBlack from "../../Assets/Logos/logo_black.svg";
+import backToTopButton from "../../Assets/Logos/back-to-top-black.svg";
 import Mail from "../Mail/Mail";
-import "jquery-ui-bundle";
+import $ from "jquery";
+// import "jquery-ui-bundle";
 
 import sanityClient from "../../client";
 
@@ -21,7 +23,16 @@ const Introduction = () => {
     })
   }, []);
 
+  const handleBackToTop = () => {
+    let targetPosition = $('body').offset().top;
+        
+    $('html, body').animate({
+      scrollTop: targetPosition
+    }, 1000);
+  }
+
   const Intro = () => {
+
     return (
       <div className="fade-in introduction" id="introduction">
         <img className="logo-black" id="logoBlack" src={logoBlack} />
@@ -36,6 +47,7 @@ const Introduction = () => {
           Full launch coming soon.
         </p>
         <Mail />
+        <img className="back-to-top" id="back-to-top-button" src={backToTopButton} onClick={handleBackToTop}/>
       </div>
     );
   };
